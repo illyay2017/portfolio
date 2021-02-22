d8f088b1f3d7e6d88e591287e210bf397758c861
Illya Yanchuk <illya.yanchuk@gmail.com>
2021-02-22 14:14:43 -0500

    added new project

---
+++ pages/scrimbaMakingWebsitesInteractive/index.js
@@ -0,0 +1,9 @@
+let myButton = document.getElementById("btn");
+var myTitle = document.getElementById("title");
+var counter = 5;
+
+myButton.addEventListener("click", function(){
+  counter += 5;
+  myButton.textContent = counter
+});
+
