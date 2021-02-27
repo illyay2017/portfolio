6fdef1d9d953a502a4ca70b5adc995fa63e8cd20
Illya Yanchuk <illya.yanchuk@gmail.com>
2021-02-26 23:13:26 -0500

    added some additional event listeners

--- pages/scrimbaMakingWebsitesInteractive/index.js
+++ pages/scrimbaMakingWebsitesInteractive/index.js
@@ -2,8 +2,10 @@ let myButton = document.getElementById("btn");
 var myTitle = document.getElementById("title");
 var counter = 5;
 
-myButton.addEventListener("click", function(){
+myButton.addEventListener("click", function(e){
+  console.log(e);
   counter += 5;
+  myButton.classList.toggle('inverted');
   myButton.textContent = counter
 });
 
